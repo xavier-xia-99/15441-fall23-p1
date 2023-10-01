81b2fc24b0d9fafb24ccdc6bee28df3740aa049e
xiabo <boxia@andrew.cmu.edu>
2023-10-01 05:15:33 +0000

    testing done

--- testing/cp1/lab_binary_tree.cpp
+++ testing/cp1/lab_binary_tree.cpp
@@ -11,14 +11,14 @@
 #include "common/testing.h"
 
 /**
- * This test-case exercises a line topology with 8 Mixnet nodes. We
- * subscribe to packet updates from every node, then send a varying
- * number of FLOOD packets using a subset of nodes as src.
+ * This test-case exercises a binary tree topology with 7 Mixnet nodes.
+ * We subscribe to packet updates from all the nodes, then send a fixed
+ * number of FLOOD packets using every node as src.
  */
-class testcase_line_hard final : public testcase {
+class testcase_tree_hard final : public testcase {
 public:
-    explicit testcase_line_hard() :
-        testcase("testcase_line_hard") {}
+    explicit testcase_tree_hard() :
+        testcase("testcase_tree_hard") {}
 
     virtual void pcap(const uint16_t, const mixnet_packet
                                 *const packet) override {
@@ -28,9 +28,17 @@ public:
     }
 
     virtual void setup() override {
-        init_graph(8);
-        graph_->generate_topology(graph::type::LINE);
-        graph_->set_mixaddrs({1, 3, 5, 4, 7, 6, 8, 0});
+        init_graph(7);
+        // Level 1
+        graph_->add_edge(0, 1);
+        graph_->add_edge(0, 2);
+        // Level 2
+        graph_->add_edge(1, 3);
+        graph_->add_edge(1, 4);
+        graph_->add_edge(2, 5);
+        graph_->add_edge(2, 6);
+        // Assign mixnet addresses
+        graph_->set_mixaddrs({52, 31, 108, 77, 23, 41, 62});
     }
 
     virtual error_code run(orchestrator& o) override {
@@ -40,12 +48,10 @@ public:
         for (uint16_t i = 0; i < graph_->num_nodes; i++) {
             DIE_ON_ERROR(o.pcap_change_subscription(i, true));
         }
-        // Try alternate nodes as source several times
-        for (uint16_t i = 0; i < graph_->num_nodes; i++) {
-            if ((i % 2) == 0) { continue; }
-
-            for (uint16_t j = 0; j < i; j++) {
-                DIE_ON_ERROR(o.send_packet(1, 0, PACKET_TYPE_FLOOD));
+        // Try every node as source
+        for (uint16_t t = 0; t < 5; t++) {
+            for (uint16_t idx = 0; idx < 7; idx++) {
+                DIE_ON_ERROR(o.send_packet(idx, 0, PACKET_TYPE_FLOOD));
             }
         }
         await_packet_propagation();
@@ -53,11 +59,11 @@ public:
     }
 
     virtual void teardown() override {
-        pass_teardown_ = (pcap_count_ == (16 * 7));
+        pass_teardown_ = (pcap_count_ == (5 * 7 * 6));
     }
 };
 
 int main(int argc, char **argv) {
-    testcase_line_hard tc; // Run testcase
+    testcase_tree_hard tc; // Run testcase
     return testcase::run_testcase(tc, argc, argv);
 }
